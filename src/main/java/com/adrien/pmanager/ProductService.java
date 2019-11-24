650678310df63b3e743f1d3544b95b72f6258a28
Adrien <adrien.dessartre@gmail.com>
2019-11-24 15:08:01 +0100

    Imports unused deleted

--- src/main/java/com/adrien/pmanager/ProductService.java
+++ src/main/java/com/adrien/pmanager/ProductService.java
@@ -3,7 +3,6 @@ package com.adrien.pmanager;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
