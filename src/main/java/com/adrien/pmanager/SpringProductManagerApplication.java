390eba12359bb2a5cf82334e45ee93869919ec87
Adrien <adrien.dessartre@gmail.com>
2019-11-24 10:59:38 +0100

    The project start

---
+++ src/main/java/com/adrien/pmanager/SpringProductManagerApplication.java
@@ -0,0 +1,13 @@
+package com.adrien.pmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrien/pmanager/SpringProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrien.pmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
